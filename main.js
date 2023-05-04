eac21efea6e0e046cc1f4668e8123bf6da3e0e30
KabirParikh <117431480+KabirParikh@users.noreply.github.com>
2023-05-04 11:42:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+var canvas;
+var video;
+var noseX=0
+var noseY=0
+function setup(){
+canvas=createCanvas(400,400);
+canvas.center();
+video=createCapture(VIDEO)
+video.hide()
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses)
+}
+
+function modelLoaded() {
+    console.log("Posenet is loaded")
+}
+
+function gotPoses(results){
+    console.log("x = "+results[0].pose.nose.x)
+    noseX= results[0].pose.nose.x
+    console.log("y = "+results[0].pose.nose.y)
+    noseY = results[0].pose.nose.y
+}
+
+
+
+
+function draw(){
+    image(video,0,0,400,400)
+    fill(255,0,0)
+    circle(noseX-140,noseY-50,20,20)
+}
+
+function take_snapshot(){
+    save("MyName.jpg");
+}
